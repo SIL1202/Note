d071acd266ffc5ea1effbe8b3fe9ee97c87983cf
劉家均 <sil@liujiajundeMacBook-Air.local>
2024-12-13 18:44:19 +0800

    add new note

---
+++ C++_Note/find_substring.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  std::string s("hello world");
+  std::string substr("ld");
+
+  if (s.substr(s.size() - substr.size()) == substr) {
+    std::cout << s.substr(s.size() - substr.size()) << std::endl;
+    std::cout << s + "s" << std::endl;
+  }
+
+  auto it = s.find(substr);
+  if (it != std::string::npos) { // 如果沒有找到就會回傳std::string::npos
+    std::cout << "found!\n";
+  } else
+    std::cout << "not found\n";
+  return 0;
+}
