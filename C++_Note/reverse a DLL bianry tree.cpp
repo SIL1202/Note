16bc57719fd3ec1c6024be62f46390bcba549eb7
劉家均 <steven60344@gmail.com>
2025-02-09 01:02:54 +0800

    add new files

--- C++_Note/reverse a DLL bianry tree.cpp
+++ C++_Note/reverse a DLL bianry tree.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+/*
 template <typename T> struct Node {
   T data;
   Node *left;
@@ -64,5 +64,88 @@ int main() {
   tree.insert(9);
   tree.insert(2);
   tree.print(); // 輸出：1 2 3
+  return 0;
+} */
+
+struct Node {
+  int data;
+  Node *next;
+  Node *prev;
+
+  Node() : next(nullptr), prev(nullptr) {}
+  Node(int d) : data(d), next(nullptr), prev(nullptr) {}
+};
+
+class DLL {
+public:
+  DLL() {
+    this->head = nullptr;
+    this->tail = nullptr;
+  }
+
+  void insert(int data) {
+    Node *newNode = new Node(data);
+    if (head == nullptr)
+      head = tail = newNode;
+    else {
+      tail->next = newNode;
+      newNode->prev = tail;
+      tail = newNode;
+    }
+  }
+
+  void remove(int data) {
+    Node *temp = head;
+    while (temp && temp->data != data) {
+      temp = temp->next;
+    }
+
+    if (!temp)
+      return;
+    if (temp == head)
+      head = temp->next;
+    if (temp == tail)
+      tail = temp->prev;
+
+    if (temp->prev)
+      temp->prev->next = temp->next;
+    if (temp->next)
+      temp->next->prev = temp->prev;
+
+    delete temp;
+  }
+
+  void printForward() {
+    Node *temp = head;
+    while (temp) {
+      std::cout << temp->data << " ";
+      temp = temp->next;
+    }
+  }
+
+  void printBackward() {
+    Node *temp = tail;
+    while (temp) {
+      std::cout << temp->data << " ";
+      temp = temp->prev;
+    }
+  }
+
+private:
+  Node *head;
+  Node *tail;
+};
+
+int main() {
+  DLL list;
+  list.insert(0);
+  list.insert(1);
+  list.insert(2);
+  list.insert(3);
+  list.insert(4);
+
+  list.printForward();
+  std::cout << "\n";
+  list.printBackward();
   return 0;
 }
