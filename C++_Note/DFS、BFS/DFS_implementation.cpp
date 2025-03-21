322216296768af93757bf72edd7935ccb2cc3a4c
劉家均 <steven60344@gmail.com>
2025-03-21 15:37:32 +0800

    add: DFS-implementation

--- C++_Note/DFS、BFS/DFS_implementation.cpp
+++ C++_Note/DFS、BFS/DFS_implementation.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <stack>
 #include <vector>
 
 void DFS(int, std::vector<std::vector<int>> &, std::vector<bool> &);
+void DFS_stk(int, std::vector<std::vector<int>> &, std::vector<bool> &);
 int main() {
   std::vector<std::vector<int>> graph = {
       {1, 2},    // node 0
@@ -14,9 +16,14 @@ int main() {
   std::vector<bool> visited(n, false);
   int start = 0;
   DFS(start, graph, visited);
+  std::cout << std::endl;
+  visited.assign(n, false);
+  DFS_stk(start, graph, visited);
   return 0;
 }
 
+// 0 1 3 4 2
+//* *
 void DFS(int node, std::vector<std::vector<int>> &graph,
          std::vector<bool> &visited) {
   std::cout << node << " ";
@@ -25,5 +32,26 @@ void DFS(int node, std::vector<std::vector<int>> &graph,
   for (int &neighbor : graph[node]) {
     if (!visited[neighbor])
       DFS(neighbor, graph, visited);
+  }
+}
+
+// 0 2 1 4 3
+void DFS_stk(int node, std::vector<std::vector<int>> &graph,
+             std::vector<bool> &visited) {
+  std::stack<int> stk;
+  stk.push(node);
+  visited[node] = true;
+
+  while (!stk.empty()) {
+    int current = stk.top();
+    stk.pop();
+    std::cout << current << " ";
+
+    for (int neighbor : graph[current]) {
+      if (!visited[neighbor]) {
+        stk.push(neighbor);
+        visited[neighbor] = true;
+      }
+    }
   }
 }
