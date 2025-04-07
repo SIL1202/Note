7c7a202391d28ccb50884bc8a7a8d1ffb17852df
劉家均 <steven60344@gmail.com>
2025-04-07 19:01:19 +0800

    fix: remove printing materials

--- C++_Note/Sorting/Quick Sort/quick sort.cpp
+++ C++_Note/Sorting/Quick Sort/quick sort.cpp
@@ -3,30 +3,21 @@
 
 using namespace std;
 
-struct QuickSortInfo {
-  vector<int> array;
-  int pivotIndex;
-  int depth;
-};
-
 void swap(int &, int &);
-void quickSort(vector<int> &, int, int, vector<QuickSortInfo> &, int);
+void quickSort(vector<int> &, int, int, int);
 int quick_partition(vector<int> &, int, int);
-void print(const vector<QuickSortInfo> &);
 
 int main() {
   int n;
   cin >> n;
   vector<int> arr(n);
   for (int i = 0; i < n; i++)
-    cin >> arr[i];
+    arr[i] = n - i;
 
-  vector<QuickSortInfo> info;
-  quickSort(arr, 0, arr.size() - 1, info, 0);
+  quickSort(arr, 0, arr.size() - 1, 0);
 
   for (int i = 0; i < arr.size(); i++)
     cout << arr[i] << " ";
-  // print(info);
   return 0;
 }
 
@@ -37,8 +28,7 @@ void swap(int &a, int &b) {
 }
 
 // QuickSort
-void quickSort(vector<int> &array, int low, int high,
-               vector<QuickSortInfo> &info, int depth) {
+void quickSort(vector<int> &array, int low, int high, int depth) {
   if (low >= high) {
     return;
   }
@@ -47,10 +37,9 @@ void quickSort(vector<int> &array, int low, int high,
 
   // 記錄當前範圍內的數組、pivot 位置、遞迴深度
   vector<int> temp(array.begin() + low, array.begin() + high + 1);
-  info.push_back({temp, pivot - low, depth});
 
-  quickSort(array, low, pivot - 1, info, depth + 1);
-  quickSort(array, pivot + 1, high, info, depth + 1);
+  quickSort(array, low, pivot - 1, depth + 1);
+  quickSort(array, pivot + 1, high, depth + 1);
 }
 
 // Partition
@@ -67,25 +56,6 @@ int quick_partition(vector<int> &array, int low, int high) {
   return i;
 }
 
-// Print function
-void print(const vector<QuickSortInfo> &info) {
-  for (const auto &entry : info) {
-    // 縮排，根據 depth 決定空格數
-    for (int i = 0; i < entry.depth * 5; i++)
-      cout << " ";
-
-    // 印出當前快排狀態，標示 pivot
-    for (int i = 0; i < entry.array.size(); i++) {
-      if (i == entry.pivotIndex)
-        cout << "[";
-      cout << entry.array[i];
-      if (i == entry.pivotIndex)
-        cout << "]";
-      cout << " ";
-    }
-    cout << endl;
-  }
-}
 /*
 
 6 3 7 5 1 2 [4]
