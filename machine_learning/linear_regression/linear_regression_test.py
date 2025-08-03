d881cd7e05907487e40182c67db06d66efe9ad1c
SIL1202 <167217913+SIL1202@users.noreply.github.com>
2025-08-03 12:40:25 +0800

    feat: add train_test_split for linear regression test

--- machine_learning/linear_regression/linear_regression_test.py
+++ machine_learning/linear_regression/linear_regression_test.py
@@ -1,18 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import datasets
+from sklearn.model_selection import train_test_split
 from linear_regression import LinearRegression as LR, loss_function
 
 iris: "datasets.Bunch" = datasets.load_iris()
 X = iris.data[:, 0]  # Sepal Length
 y = iris.data[:, 3]  # Petal Width
 
+# split datasets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=20)
+
 model = LR()
-loss_function = model.fit(X, y, lr=0.01, iters=500)
+loss_function = model.fit(X_train, y_train, lr=0.01, iters=500)
 
 print(f"Final weight: {model.weight}, Final bias: {model.bias}")
 print(f"Final loss: {loss_function[-1]}")
-print(f"predict patal width: {model.predict(4.5)}")
+print(f"predict patal width: {model.predict(X_test)}")
+
 
 # draw the learning curve
 plt.plot(range(len(loss_function)), loss_function, color="purple")
@@ -23,7 +28,7 @@ plt.grid(True)
 plt.show()
 
 # draw scatter information
-plt.scatter(X, y, color="blue", label="data points")
+plt.scatter(X_train, y_train, color="blue", label="data points")
 
 x_line = np.linspace(min(X), max(X), 100)
 # give the Sepal Length and predict the Petal Width
