1c27d0a1fabc4b39efe38fec86e1ffabf4dfe204
SIL1202 <167217913+SIL1202@users.noreply.github.com>
2025-08-20 22:17:31 +0800

    refactor: rename files and adjust port number.

--- C++_Note/operating_system/socket/RPC/server.c
+++ C++_Note/operating_system/socket/RPC/server.c
@@ -4,18 +4,18 @@
 #include <string.h>
 #include <unistd.h>
 
-#define PORT 9000
-#define BUFSZ 1024
-
+#define BUFSZ 256
+#define REG_PORT 8888
 long func(const long a, const long b) { return a * b; }
 
 int main() {
+  // (a) 綁 0 由 OS 配一個臨時埠
   int srv = socket(AF_INET, SOCK_STREAM, 0);
-  struct sockaddr_in addr = {0};
-  addr.sin_family = AF_INET;
-  addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  addr.sin_port = htons(PORT);
-  if (bind(srv, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+  struct sockaddr_in a = {0};
+  a.sin_family = AF_INET;
+  a.sin_addr.s_addr = htonl(INADDR_ANY);
+  a.sin_port = htons(0);
+  if (bind(srv, (struct sockaddr *)&a, sizeof(a)) < 0) {
     perror("bind");
     exit(1);
   }
@@ -24,7 +24,22 @@ int main() {
     exit(1);
   }
 
-  printf("RPC server listening on port %d\n", PORT);
+  // Get the port number assigned by the OS
+  socklen_t len = sizeof(a);
+  int port = ntohs(a.sin_port);
+  if (getsockname(srv, (struct sockaddr *)&a, &len) < 0) {
+    perror("getsockname");
+    exit(1);
+  }
+  printf("service 'add' on port %d\n", port);
+
+  // (b) 向 registry 註冊
+  int r = socket(AF_INET, SOCK_STREAM, 0);
+  struct sockaddr_in ra = {0};
+  ra.sin_family = AF_INET;
+  ra.sin_port = htons(REG_PORT);
+  inet_pton(AF_INET, "127.0.0.1", &ra.sin_addr);
+
   for (;;) {
     int cli = accept(srv, NULL, NULL);
     if (cli < 0) {
